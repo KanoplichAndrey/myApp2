d8da3005d19c0bd0cb50a314b910bee82bd85a7c
KanoplichAndrey <kanop0000@gmail.com>
2023-01-29 23:17:34 +0300

    x

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
         {/* {mobile ? <BurgerMenu /> : <Headerr />} */}
         <Home />
         <Routes>
-          <Route path="myApp2/" element={<Home />} />
+          <Route path="kanoplichandrey.github.io/myApp2/" element={<Home />} />
           <Route path="Создание сайтов" element={<Pages />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
